4329458173ea3b8e0deb09d1cbf4482336696de1
memetsaranur <nurmemetkasgarli@gmail.com>
2025-12-07 04:13:22 +0300

    Disable ESLint for Vercel build

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -7,6 +7,9 @@ const nextConfig = {
   reactStrictMode: true,
   // Note: i18n configuration is not compatible with App Router
   // Using custom client-side i18n solution via LanguageProvider instead
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 module.exports = withPWA(nextConfig);
